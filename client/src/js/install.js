07f9909e003061d7e157c3d8f31ff422ab963a94
KT <132851569+kumih0@users.noreply.github.com>
2023-07-19 15:54:32 -0500

    adding event handlers in install file and ref mp

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -5,15 +5,31 @@ const butInstall = document.getElementById('buttonInstall');
 window.addEventListener('beforeinstallprompt', (event) => {
     // Store the triggered events
     window.deferredPrompt = event;
-
     // Remove the hidden class from the button.
     butInstall.classList.toggle('hidden', false);
 });
 
 // TODO: Implement a click event handler on the `butInstall` element
 butInstall.addEventListener('click', async () => {
-    
+    // Retrieve the deferred prompt
+    const promptEvent = window.deferredPrompt;
+    // If there is no prompt, exit the function
+    if (!promptEvent) {
+        return;
+    }
+    // Show prompt
+    promptEvent.prompt();
+    // Reset the deferred prompt variable, it can only be used once.
+    window.deferredPrompt = null;
+    // Hide the install button.
+    butInstall.classList.toggle('hidden', true);
 });
 
 // TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+window.addEventListener('appinstalled', (event) => {
+    // Clear prompt
+    window.deferredPrompt = null;
+    // Log the installation to analytics or save the event somehow.
+    console.log('👍', 'appinstalled', event);
+
+});
