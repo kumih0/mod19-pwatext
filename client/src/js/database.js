576fded125167d7daa11da298625ec0f058c4d7c
KT <132851569+kumih0@users.noreply.github.com>
2023-07-19 18:08:43 -0500

    correcting putdb and running stuff

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -14,22 +14,17 @@ const initdb = async () =>
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 //export function to PUT to database
-export const putDb = async (content) => {
-  console.log('Put to the database');
-
+export const putDb = async (id, content) => {
+  console.log('PUT to the database');
   // Create a connection to the database database and version we want to use.
   const jateDb = await openDB('jate', 1);
-
   // Create a new transaction and specify the database and data privileges.
   const tx = jateDb.transaction('jate', 'readwrite');
-
   // Open up the desired object store.
   const store = tx.objectStore('jate');
-
-  // Use the .add() method on the store and pass in the content.
-  const request = store.add(content);
-
-  // Get confirmation of the request.
+  // Use the .put() method to add data to the database.
+  const request = store.put({ id: id, todo: content });
+  // Get confirmation of the request. 
   const result = await request;
   console.log('🚀 - data saved to the database', result);
 };
